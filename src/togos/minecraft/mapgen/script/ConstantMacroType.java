41104d9fde37de8e4def50c9d4d36560ff1018c3
TOGoS <togos00@gmail.com>
2010-11-27 22:57:40 -0600

    ConstantMacroType now throws CompileError if it's called with arguments.

--- src/togos/minecraft/mapgen/script/ConstantMacroType.java
+++ src/togos/minecraft/mapgen/script/ConstantMacroType.java
@@ -7,6 +7,9 @@ public class ConstantMacroType implements MacroType
 		this.value = value;
 	}
 	public Object instantiate(ScriptCompiler c, ScriptNode sn) {
+		if( sn.arguments.size() > 0 ) {
+			throw new CompileError(sn.macroName + " takes no arguments, "+sn.arguments.size()+" given", sn);
+		}
 		return value;
 	}
 }
