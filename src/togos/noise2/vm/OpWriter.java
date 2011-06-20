a674d6fa88995f250a64c3fcca6543f83d181999
TOGoS <togos00@gmail.com>
2011-06-20 00:53:16 -0500

    Updated comments in OpWriter.

--- src/togos/noise2/vm/OpWriter.java
+++ src/togos/noise2/vm/OpWriter.java
@@ -2,11 +2,12 @@ package togos.noise2.vm;
 
 public interface OpWriter
 {
-	/**
-	 * @param type common name for data type, e.g. 'double', 'boolean', 'double[]', ...
-	 * @param resNameSuggestion
-	 * @return
-	 */
+	// @param dest usually means the variable (must already be declared, if non-null)
+	// in which to put the resulting value.  But the writer may decide to put the value
+	// into a new value or to inline any expression.
+	
+	// @param type common name for data type, e.g. 'double', 'boolean', 'double[]', ...
+	
 	public String declareInput( String type, String nameSuggestion );
 	public String declareVar( String type, String nameSuggestion );
 	public String writeConstant( String dest, double value  );
