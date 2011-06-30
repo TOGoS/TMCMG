c17aa978531f11ed17651899c8aa64b0cc21f4e6
TOGoS <togos00@gmail.com>
2011-06-30 01:47:11 -0500

    Added Exponentiate terms to TNLNamespace.

--- src/togos/noise2/rdf/TNLNamespace.java
+++ src/togos/noise2/rdf/TNLNamespace.java
@@ -11,7 +11,7 @@ public class TNLNamespace
 	public static final String ADD  = TN_NS+"Add";
 	public static final String TERM = TN_NS+"term";
 	
-	public static final String SUBTRACT = TN_NS+"Subtract";
+	public static final String SUBTRACT   = TN_NS+"Subtract";
 	public static final String MINUEND    = TN_NS+"minuend";
 	public static final String SUBTRAHEND = TN_NS+"subtrahend";
 
@@ -21,4 +21,8 @@ public class TNLNamespace
 	public static final String DIVIDE   = TN_NS+"Divide";
 	public static final String DIVIDEND = TN_NS+"dividend";
 	public static final String DIVISOR  = TN_NS+"divisor";
+	
+	public static final String Exponentiate = TN_NS+"Exponentiate";
+	public static final String BASE         = TN_NS+"base";
+	public static final String EXPONENT     = TN_NS+"exponent";
 }
