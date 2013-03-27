e54bc3cb930736a96b90840b7aad52a393e324f6
TOGoS <togos00@gmail.com>
2013-03-27 09:43:11 -0500

    Make CoolTestCase abstract so JUnit doesn't try to run it.

--- test/togos/noise/v3/parser/CoolTestCase.java
+++ test/togos/noise/v3/parser/CoolTestCase.java
@@ -2,7 +2,7 @@ package togos.noise.v3.parser;
 
 import junit.framework.TestCase;
 
-public class CoolTestCase extends TestCase {
+public abstract class CoolTestCase extends TestCase {
 	protected void assertInstanceOf( Class<?> c, Object obj ) {
 		assertNotNull( "Expected non-null value", obj );
 		assertTrue( "Expected instance of "+c+", but got "+obj.getClass(), c.isAssignableFrom(obj.getClass()) );
