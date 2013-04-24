723656621de9920d0234789204904960e11946be
TOGoS <togos00@gmail.com>
2013-04-23 20:19:25 -0500

    Make LinkedListNode implement Iterable for some reason.

--- src/togos/noise/v3/program/runtime/LinkedListNode.java
+++ src/togos/noise/v3/program/runtime/LinkedListNode.java
@@ -1,31 +1,35 @@
 package togos.noise.v3.program.runtime;
 
+import java.util.Iterator;
 import java.util.List;
 
-public class LinkedListNode<V> {
+/**
+ * Simple read-only linked list.
+ */
+public class LinkedListNode<V> implements Iterable<V> {
 	public static final LinkedListNode<?> EMPTY = new LinkedListNode<Object>(); 
 	
-	public final boolean isEmpty; 
+	public final int length; 
 	public final V head;
 	public final LinkedListNode<? extends V> tail;
 	public LinkedListNode( V head, LinkedListNode<? extends V> tail ) {
-		this.isEmpty = false;
+		this.length = tail.length + 1;
 		this.head = head;
 		this.tail = tail;
 	}
 	
 	private LinkedListNode() {
-		this.isEmpty = true;
+		this.length = 0;
 		this.head = null;
 		this.tail = this;
 	}
 	
 	public String toString() {
-		if( this.isEmpty ) return "[]";
+		if( this.length == 0 ) return "[]";
 		
 		String res = null;
 		LinkedListNode<? extends V> n = this;
-		while( !n.isEmpty ) {
+		while( n.length > 0 ) {
 			res = (res == null ? "[\n" : res + ",\n") + "\t" + n.head;
 			n = n.tail;
 		}
@@ -48,5 +52,31 @@ public class LinkedListNode<V> {
 			tail = new LinkedListNode<V>( values.get(i), tail );
 		}
 		return tail;
+	}
+	
+	////
+	
+	// A nice advantage of linked lists is that you can get by without
+	// iterators.  But if you really want one...
+	@Override public Iterator<V> iterator() {
+		return new Iterator<V>() {
+			LinkedListNode<? extends V> n = LinkedListNode.this;
+			
+			@Override public boolean hasNext() {
+				return n.length > 0;
+			}
+			
+			@Override public V next() {
+				try {
+					return n.head;
+				} finally {
+					n = n.tail;
+				}
+			}
+			
+			@Override public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
 	}
 }
