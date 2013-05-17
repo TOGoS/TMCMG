d6142597bc6e8e690d396980e5b2043ed03c2646
TOGoS <togos00@gmail.com>
2013-05-16 19:38:28 -0500

    Fixed floored division modulus.

--- src/togos/noise/MathUtil.java
+++ src/togos/noise/MathUtil.java
@@ -3,15 +3,8 @@ package togos.noise;
 public class MathUtil {
 	public static final double safeFlooredDivisionModulus( double num, double den ) {
 		if( num == 0 || den == 0 ) return 0;
-		else {
-			// TODO: this is wrong (see unit test results); fix
-			boolean invert = false;
-			if( num < 0 ) invert = !invert;
-			if( den < 0 ) invert = !invert;
-			double v1 = invert ? -num : num;
-			double factor = Math.floor(v1 / den);
-			v1 -= factor*den;
-			return invert ? -v1 : v1;
-		}
+		
+		double s = Math.floor(num / den);
+		return num - s*den;
 	}
 }
